868266d9c7d8332c4cd9c02e6a44eb8015d56ad4
Rogger-Vera <85507115+Rogger-Vera@users.noreply.github.com>
2021-10-07 04:00:00 -0300

    Add files via upload

---
+++ Desafio01_RoggerVera.py
@@ -0,0 +1,10 @@
+# Desafio 01 - Rogger Vera
+# Se necesita listar los datos de 5 personas usando un formato de columnas con un titulo subrayado con guiones comunes.
+# Los datos a listar de las personas son Nombre, Apellido, Edad y email.
+print('Nombre','Apellido','Edad','Email', sep=' \t ')
+print('------','--------','----','-----', sep=' \t ')
+print('Jorge','Alvarez',45,'jalvarez45@gmail.com', sep=' \t ')
+print('Maria','Alvarez',43,'malvarez43@gmail.com', sep=' \t ')
+print('Carlos','Alvarez',20,'calvarez20@gmail.com', sep=' \t ')
+print('Jimena','Alvarez',16,'jalvarez16@gmail.com', sep=' \t ')
+print('Camila','Alvarez',15,'calvarez15@gmail.com', sep=' \t ')
